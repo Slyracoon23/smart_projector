f050163107e4ed20d80454d3888270186dc75d6d
Earl Potters <earl.potters@gmail.com>
2025-02-06 17:01:47 -0800

    Rebrand static website content to SmartProjector platform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,15 @@ const iframeContent = {
     mainHeading: '.MuiTypography-h1',
     serviceDescription: '.check-list p',
     logo: '.css-1p8i6qr',
+    subHeading: '.css-1l3awos',
+    subDescription: '.css-1tl8prx',
   },
   content: {
     mainHeading: "The best way to manage contractors across the globe",
     serviceDescription: "Manage contractors easier than ever. Even minimize your misclassification risks with delayed projects and missed deadlines. automate onboarding, managing and tracking in minutes.",
     logo: '/SmartProjector.svg',
+    subHeading: "The only project management platform with everything you need, for everyone",
+    subDescription: "We're making global team management so easy, businesses don't need to think twice. Whether you want to hire contractors in the Pakistan, India, or Ukraine — SmartProjector makes the whole process automatic and simple (while keeping you efficient)",
   }
 };
 
